5c85d44a14ae10a628d92b7731db7781747116cb
Dob <lalilulelo.mg@gmail.com>
2023-06-09 13:23:48 +1000

    added small transition to hire me hoover effect

--- src/app/components/HireMe/HireMe.tsx
+++ src/app/components/HireMe/HireMe.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { CircularSVG } from '../icons/icons'
 import styles from './hireMe.module.css'
 
@@ -6,7 +7,10 @@ export default function HireMe() {
     <div className={styles.hireMe} >
         <div className={styles.svgContainer}>
             <CircularSVG />
+            <Link href="/" className={styles.hireLink} >Hire Me</Link>
         </div>
     </div>
   )
 }
+
+//TODO : Add a link to the contact page or email me directly
